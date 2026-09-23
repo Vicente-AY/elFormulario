1cd630b6405df0535222e3b8dcc254eaf5cd3911
ALBEROLA YAGÜE <vicalbyag@alu.edu.gva.es>
2026-09-23 18:38:11 +0200

    Creada estructura general

---
+++ elFormulario/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace elFormulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnArriba = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // btnArriba
+            // 
+            btnArriba.Location = new Point(138, 97);
+            btnArriba.Name = "btnArriba";
+            btnArriba.Size = new Size(75, 23);
+            btnArriba.TabIndex = 0;
+            btnArriba.Text = "Arriba";
+            btnArriba.UseVisualStyleBackColor = true;
+            btnArriba.Click += btnArriba_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(108, 199);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Izquierda";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(255, 185);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 2;
+            button3.Text = "Derecha";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(176, 294);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 3;
+            button4.Text = "Abajo";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(242, 263);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 4;
+            button5.Text = "Crea Botón";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(274, 97);
+            button6.Name = "button6";
+            button6.Size = new Size(155, 23);
+            button6.TabIndex = 5;
+            button6.Text = "Pon no Visible";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(btnArriba);
+            Name = "Form1";
+            Text = "ElFormulario";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnArriba;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ elFormulario/Form1.cs
@@ -0,0 +1,20 @@
+namespace elFormulario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnArriba_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Holiwi UwU");
+        }
+    }
+}
